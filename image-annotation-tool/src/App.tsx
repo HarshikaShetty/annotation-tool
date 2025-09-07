f09d05cbc23c6658c565c3e73df52d37631b5f2a
harshika.k-at-411977029542 <harshika.k@juegostudioz.com>
2025-09-07 10:06:42 +0530

    Implement image capture functionality and update image gallery

--- image-annotation-tool/src/App.tsx
+++ image-annotation-tool/src/App.tsx
@@ -1,10 +1,46 @@
+import { useCallback, useRef, useState } from 'react'
+import type Webcam from 'react-webcam';
+import { useSetAtom } from 'jotai';
+
 import './App.css'
+import './index.css'
+
+import CaptureView from './components/captureView'
+import ImageGallery from './components/imageGallery';
+import { imagesAtom, showWebCamAtom } from './store/atom';
+
 
 function App() {
 
-  return (
-    <div className='w-full h-screen flex items-center justify-center'>
+  const webcamRef = useRef<Webcam>(null);
+  const [selectedImageId, setSelectedImageId] = useState<string | null>('');
+  const setImages = useSetAtom(imagesAtom);
+  const setShowWebCam = useSetAtom(showWebCamAtom);
 
+  const handleCaptureImage = useCallback(() => {
+    const imageSrc = webcamRef.current?.getScreenshot();
+    if (imageSrc) {
+      const newImage: IImage = {
+        id: Date.now().toString(),
+        src: imageSrc,
+        name: `image-${Date.now()}.jpeg`,
+        annotations: []
+      }
+      setImages(prevImages => [...prevImages, newImage]);
+    }
+  }, [setImages])
+
+  const handleSelectImage = useCallback((id: string | null) => {
+    setSelectedImageId(id || null);
+    setShowWebCam(!id);
+  }, [setShowWebCam])
+
+  return (
+    <div className='w-full h-screen flex items-center justify-between p-4'>
+      <div className='w-[70%] h-full flex flex-col gap-12 bg-gray-100 rounded-2xl'>
+        <CaptureView webcamRef={webcamRef} onCapture={handleCaptureImage} />
+        <ImageGallery handleSelectImage={handleSelectImage} selectedImageId={selectedImageId} />
+      </div>
     </div>
   )
 }
