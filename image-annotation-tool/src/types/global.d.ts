972dc25c03d53e6872ba3fc466a2ef21db80e552
harshika.k-at-411977029542 <harshika.k@juegostudioz.com>
2025-09-10 00:08:35 +0530

    Added global types for annotation

--- image-annotation-tool/src/types/global.d.ts
+++ image-annotation-tool/src/types/global.d.ts
@@ -1,12 +1,24 @@
 // Make this file a module so TS includes it
 export {};
-
-// Example: declare global interfaces
 declare global {
+  interface IAnnotation {
+    id: number | string;
+    type: string;
+    label: string;
+    color?: string;
+    categories: string[];
+    phi: number;
+    getCenterWithOffset: () => { X: number; Y: number };
+  }
   interface IImage {
     id: string;
     src: string;
     name: string;
-    annotations?: unknown[];
+    annotations?: IAnnotation[];
+  }
+
+  interface IAnnotateObject {
+    id: number;
+    name: string;
   }
 }
