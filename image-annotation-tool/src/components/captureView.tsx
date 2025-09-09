6796cad03d70d7b0e2befd5c86766e02fc945bd0
harshika.k-at-411977029542 <harshika.k@juegostudioz.com>
2025-09-10 00:11:39 +0530

    update: Resuse of button component in Capture view

--- image-annotation-tool/src/components/captureView.tsx
+++ image-annotation-tool/src/components/captureView.tsx
@@ -2,6 +2,9 @@ import { useAtomValue } from "jotai";
 import React, { memo, type FC } from "react";
 import { FaCamera } from "react-icons/fa";
 import Webcam from "react-webcam";
+
+import Button from "./shared/button";
+
 import { showWebCamAtom } from "../store/atom";
 
 interface ICaptureViewProps {
@@ -13,7 +16,7 @@ const CaptureView: FC<ICaptureViewProps> = ({ onCapture, webcamRef }) => {
     const showWebCam = useAtomValue(showWebCamAtom);
     if (!showWebCam) return null;
     return (
-        <div className="flex items-center gap-8 w-full h-[60%] px-4">
+        <div className="media-panel gap-2">
             <div className="w-full h-full">
                 <Webcam
                     ref={webcamRef}
@@ -22,12 +25,12 @@ const CaptureView: FC<ICaptureViewProps> = ({ onCapture, webcamRef }) => {
                 />
             </div>
             <div className="flex gap-4">
-                <button
-                    className="bg-blue-500 hover:bg-blue-600 p-4 rounded-full flex items-center justify-center cursor-pointer"
+                <Button
+                    className="bg-purple-700 hover:bg-purple-600 p-8 rounded-full flex items-center justify-center"
                     onClick={onCapture}
                 >
-                    <FaCamera size={20} fill="#fff" />
-                </button>
+                    <FaCamera size={30} fill="#fff" />
+                </Button>
             </div>
         </div>
     );
