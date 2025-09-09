b225e2ceeacb2d34c67e1c306acb9f9e773126d5
harshika.k-at-411977029542 <harshika.k@juegostudioz.com>
2025-09-10 00:10:09 +0530

    feat:Save images atom with session storage

--- image-annotation-tool/src/store/atom.ts
+++ image-annotation-tool/src/store/atom.ts
@@ -1,4 +1,20 @@
 import { atom } from "jotai";
+import { atomWithStorage } from "jotai/utils";
 
-export const imagesAtom = atom<IImage[]>([]);
+export const imagesAtom = atomWithStorage<IImage[]>(
+  "images", // key for sessionStorage
+  [], // initial value
+  {
+    getItem: (key) => {
+      const stored = sessionStorage.getItem(key);
+      return stored ? (JSON.parse(stored) as IImage[]) : [];
+    },
+    setItem: (key, newValue) => {
+      sessionStorage.setItem(key, JSON.stringify(newValue));
+    },
+    removeItem: (key) => {
+      sessionStorage.removeItem(key);
+    },
+  }
+);
 export const showWebCamAtom = atom<boolean>(true);
